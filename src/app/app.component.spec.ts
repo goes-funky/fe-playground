97b24bf9ee6d0109bb77c043c85127d557c71841
Alexander Ciesielski <alexander@ciesielski.co>
2022-07-25 17:16:56 +0200

    ci

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MockComponents } from 'ng-mocks';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 describe(AppComponent.name, () => {
   let component: AppComponent;
@@ -9,7 +11,7 @@ describe(AppComponent.name, () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [AppComponent, MockComponents(NavigationComponent)],
       providers: [],
     }).compileComponents();
   });
