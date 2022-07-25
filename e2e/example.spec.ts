e2a5f11c52d35accd7490cf471839fc93ea3d23e
Alexander Ciesielski <alexander@ciesielski.co>
2022-07-25 14:44:35 +0200

    chore: init playwright

---
+++ e2e/example.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from '@playwright/test';
+
+test('homepage has Y42 in title and get started link linking to the intro page', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Y42/);
+});
