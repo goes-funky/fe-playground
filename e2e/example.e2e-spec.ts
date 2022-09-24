4bf280e01bdfdea197c9c7990ad3e20132d3fac3
Islam404 <islam_magdy1@outlook.com>
2022-09-24 14:59:59 +0200

    commits

--- e2e/example.e2e-spec.ts
+++ e2e/example.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('homepage has Y42 in title and get started link linking to the intro page', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('www.google.com');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Y42/);
