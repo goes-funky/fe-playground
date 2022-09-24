a8500c2117cb7acee8a47484fe3477d5a2c83fb3
Islam404 <islam_magdy1@outlook.com>
2022-09-24 21:39:24 +0200

    final commit

--- e2e/example.e2e-spec.ts
+++ e2e/example.e2e-spec.ts
@@ -1,8 +1,72 @@
 import { expect, test } from '@playwright/test';
 
-test('homepage has Y42 in title and get started link linking to the intro page', async ({ page }) => {
-  await page.goto('www.google.com');
 
-  // Expect a title "to contain" a substring.
+
+
+test('Login', async ({ page }) => {
+  await page.goto('/');
   await expect(page).toHaveTitle(/Y42/);
+  await page.locator('text=email').fill("Islam@yahoo.com")
+  await page.locator('text=Password').fill("12")
+  await page.locator('button').first().isEnabled()
+  await page.locator('button').first().click()
+
+  
 });
+
+test('Product form', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/Y42/);
+  await page.locator('text=email').fill("Islam@yahoo.com")
+  await page.locator('text=Password').fill("12")
+  await page.locator('button').first().isEnabled()
+  await page.locator('button').first().click()
+  await page.waitForSelector("text=- Daal Masoor 500 grams",{timeout:10000}); 
+  const products = await page.$$('xpath =//*[@aria-colindex="1"]'); 
+  await products.at(1)?.dblclick();
+  await page.screenshot({ path: 'screenshot.png' });
+
+});
+
+test('Stock Editing', async ({ page }) => {
+
+
+  await page.goto('/');
+  await expect(page).toHaveTitle(/Y42/);
+  await page.locator('text=email').fill("Islam@yahoo.com")
+  await page.locator('text=Password').fill("12")
+  await page.locator('button').first().isEnabled()
+  await page.locator('button').first().click();
+  await page.waitForSelector("text=- Daal Masoor 500 grams",{timeout:10000}); 
+  const stocks = await page.$$('xpath =//*[@aria-colindex="4"]'); 
+  const stock1 = await stocks.at(1)?.dblclick();
+  await stocks.at(1)?.type('');
+  await stocks.at(1)?.type('125');
+  await page.keyboard.press('Enter');
+  await page.screenshot({ path: 'screenshot.png' });
+  //console.log(stocks.values()); 
+  console.log( stocks.length); 
+ 
+});
+
+
+test('Price Editing', async ({ page }) => {
+
+  await page.goto('/');
+  await expect(page).toHaveTitle(/Y42/);
+  await page.locator('text=email').fill("Islam@yahoo.com")
+  await page.locator('text=Password').fill("12")
+  await page.locator('button').first().isEnabled()
+  await page.locator('button').first().click()
+  await page.waitForSelector("text=- Daal Masoor 500 grams",{timeout:10000}); 
+  const stocks = await page.$$('xpath =//*[@aria-colindex="5"]'); 
+  const stock1 = await stocks.at(1)?.dblclick();
+  await stocks.at(1)?.type('');
+  await stocks.at(1)?.type('125');
+  await page.keyboard.press('Enter');
+  await page.screenshot({ path: 'screenshot.png' });
+  //console.log(stocks.values()); 
+  console.log( stocks.length); 
+ 
+});
+
