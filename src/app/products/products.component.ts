8a57116af79325671d958a7e27c04a519374eb70
mohamed <ata@innosoft.me>
2022-10-07 19:28:03 +0200

    implement add new product and implement search products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
-
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { filter, switchMap } from 'rxjs';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { Product } from './product-http.service';
+import { ProductService } from './product.service';
 @Component({
   selector: 'y42-products',
   template: `
     <div style="height: 100%; padding: 1rem">
-      <h2>Products</h2>
+      <div class="d-flex justify-content-between">
+        <h2>Products</h2>
+        <button mat-button color="primary" type="button" (click)="openAddNew()">+ Add New</button>
+      </div>
       <router-outlet></router-outlet>
     </div>
   `,
+  styles: [
+    `
+      :host{
+        .d-flex{
+          display: flex;
+        }
+        .justify-content-between{
+          justify-content: space-between;
+          align-items: center;
+        }
+      }
+    `
+  ]
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+  constructor(private productService: ProductService, private bottomSheet: MatBottomSheet) {}
+
+  openAddNew(): void {
+    this.bottomSheet
+      .open<ProductDetailComponent, Product, Product>(ProductDetailComponent)
+      .afterDismissed()
+      .pipe(
+        filter(Boolean),
+        switchMap((newProduct) => this.productService.addProduct(newProduct)),
+      )
+      .subscribe();
+      
+  }
+}
